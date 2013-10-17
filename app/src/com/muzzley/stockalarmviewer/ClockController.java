32f623848d73637ec251abbf3811e495d34f2812
Filipe Pinheiro <me@fampinheiro.com>
2013-10-17 11:08:49 +0100

    Changed parse method to comtemplate dates in format h'h'mm

--- app/src/com/muzzley/stockalarmviewer/ClockController.java
+++ app/src/com/muzzley/stockalarmviewer/ClockController.java
@@ -1,5 +1,18 @@
 package com.muzzley.stockalarmviewer;
 
+import static android.provider.Settings.NameValueTable.VALUE;
+import static android.provider.Settings.System.NEXT_ALARM_FORMATTED;
+import static java.util.Calendar.DAY_OF_WEEK;
+import static java.util.Calendar.HOUR_OF_DAY;
+import static java.util.Calendar.MINUTE;
+import static java.util.Calendar.SECOND;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,34 +22,33 @@ import android.net.Uri;
 import android.os.Handler;
 import android.provider.AlarmClock;
 import android.provider.Settings;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
-import static android.provider.Settings.System.NEXT_ALARM_FORMATTED;
-import static android.provider.Settings.System.VALUE;
-import static java.util.Calendar.DAY_OF_WEEK;
-import static java.util.Calendar.HOUR_OF_DAY;
-import static java.util.Calendar.MINUTE;
-import static java.util.Calendar.SECOND;
+import android.util.Log;
 
 public class ClockController {
-  private static final Uri URI = Settings.System.getUriFor(NEXT_ALARM_FORMATTED);
-  private static final int[] FIELDS = new int[]{DAY_OF_WEEK, HOUR_OF_DAY, MINUTE, SECOND};
-  private static final DateFormat FORMAT = new SimpleDateFormat("EEE HH:mm");
+  private static final Uri URI;
+  private static final int[] FIELDS;
   private final ContentResolver resolver;
   private final ContentObserver observer;
   private final Context context;
   private Listener listener;
 
+  private final static String DM12;
+  private final static String DM24;
+  private static final String TAG = "com.muzzley.stockalarmviewer";
+
+  static {
+    URI = Settings.System.getUriFor(NEXT_ALARM_FORMATTED);
+    FIELDS = new int[] { DAY_OF_WEEK, HOUR_OF_DAY, MINUTE, SECOND };
+    DM12 = "E h:mm aa";
+    DM24 = "E k:mm";
+  }
+
   public ClockController(Context context, Handler handler) {
     this.context = context;
     this.resolver = context.getContentResolver();
     this.observer = new ContentObserver(handler) {
-      @Override public void onChange(boolean selfChange) {
+      @Override
+      public void onChange(boolean selfChange) {
         super.onChange(selfChange);
         notifyListener();
       }
@@ -48,7 +60,26 @@ public class ClockController {
   }
 
   private void notifyListener() {
-    if (listener != null) listener.onChange();
+    if (listener != null)
+      listener.onChange();
+  }
+
+  private static Date parseDayAndTime(final Context context, String timeString) {
+    String format = get24HourMode(context) ? DM24 : DM12;
+    SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.getDefault());
+    try {
+      return sdf.parse(timeString);
+    } catch (ParseException e) {
+      format = (get24HourMode(context) ? DM24 : DM12).replace(":", "'h'");
+      sdf = new SimpleDateFormat(format, Locale.getDefault());
+      Log.d(TAG, e.getMessage(), e);
+      try {
+        return sdf.parse(timeString);
+      } catch (ParseException pe) {
+        Log.d(TAG, e.getMessage(), e);
+        return null;
+      }
+    }
   }
 
   public Calendar getNextAlarm() {
@@ -57,21 +88,19 @@ public class ClockController {
       try {
         if (c.getCount() > 0) {
           c.moveToFirst();
-          try {
-            Date dt = FORMAT.parse(c.getString(c.getColumnIndex(VALUE)));
-            Calendar now = Calendar.getInstance();
-            now.setTime(dt);
-
-            // Since the date returned is of format 'EEE HH:mm'
-            // we need to inject the other values based on the date right now
-            Calendar al = Calendar.getInstance();
-            for (int field : FIELDS) {
-              al.set(field, now.get(field));
-            }
-            return al;
-          } catch (ParseException e) {
-            return null;
+          Calendar now = Calendar.getInstance();
+          Date dt = parseDayAndTime(context, c.getString(c.getColumnIndex(VALUE)));
+          
+          if (dt == null) return null;
+          
+          // Since the date return only the time and day of the week
+          // we need to inject the other values based on the date
+          // right now
+          Calendar al = Calendar.getInstance();
+          for (int field : FIELDS) {
+            al.set(field, now.get(field));
           }
+          return al;
         }
       } finally {
         c.close();
@@ -88,12 +117,16 @@ public class ClockController {
     resolver.unregisterContentObserver(observer);
   }
 
+  static boolean get24HourMode(final Context context) {
+    return android.text.format.DateFormat.is24HourFormat(context);
+  }
+
   public void setAlarm(Calendar calendar) {
     Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-      .putExtra(AlarmClock.EXTRA_HOUR, calendar.get(Calendar.HOUR_OF_DAY))
-      .putExtra(AlarmClock.EXTRA_MINUTES, calendar.get(Calendar.MINUTE))
-      .putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+        .putExtra(AlarmClock.EXTRA_HOUR, calendar.get(Calendar.HOUR_OF_DAY))
+        .putExtra(AlarmClock.EXTRA_MINUTES, calendar.get(Calendar.MINUTE))
+        .putExtra(AlarmClock.EXTRA_SKIP_UI, true);
     context.startActivity(intent);
   }
 
